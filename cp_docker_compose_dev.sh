4ab28a17de2605246e31555f245599d1be46ea81
Yoel Monzon <yoelfme@hotmail.com>
2016-03-14 19:18:45 -0600

    [add] scripts for docker

---
+++ cp_docker_compose_dev.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cp .docker/docker-compose/docker-compose-dev.yml docker-compose.yml
---
+++ cp_docker_compose_pro.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cp .docker/docker-compose/docker-compose-pro.yml docker-compose.yml
