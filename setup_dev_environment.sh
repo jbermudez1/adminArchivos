f8f3c12661d5a2dcbb2d0fcf86356386f7e3d8dd
Yoel Monzon <yoelfme@hotmail.com>
2016-03-16 02:08:09 -0600

    [update] scripts of configuration

--- setup_dev_environment.sh
+++ setup_dev_environment.sh
@@ -2,7 +2,6 @@
 ./cp_docker_compose_dev.sh
 ./cp_env_dev.sh
 chmod -R 777 storage
-chmod -R 777 bootstrap/cache
 docker-compose build
 docker-compose stop
 docker-compose rm --force
@@ -10,6 +9,3 @@ docker-compose up -d
 docker-compose run composer install
 docker-compose run artisan key:generate
 docker-compose run artisan migrate
-docker-compose run artisan update:products
-docker-compose run artisan update:cities
-docker-compose run artisan update:suburbs
--- setup_prod_environment.sh
+++ setup_prod_environment.sh
@@ -2,13 +2,9 @@
 ./cp_docker_compose_pro.sh
 ./cp_env_prod.sh
 chmod -R 777 storage
-chmod -R 777 bootstrap/cache
 docker-compose build
 docker-compose stop
 docker-compose up -d --force-recreate
 docker-compose run composer install
 docker-compose run artisan key:generate
 docker-compose run artisan migrate
-docker-compose run artisan update:products
-docker-compose run artisan update:cities
-docker-compose run artisan update:suburbs
